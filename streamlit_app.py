3b663bed1a7e7087a507f8d51e7a9c1dd304570f
Andrew Li <lawfairachyt28@gmail.com>
2023-04-27 14:56:50 -0400

    Secrets Day 17
    - secrets.toml is only for local testing.
    
    - Secrets can be uploaded directly in streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,14 @@
 import streamlit as st
 
-st.title('Customizing the theme of Streamlit apps')
+# Everything is accessible via the st.secrets dict:
 
-st.write('Contents of the `.streamlit/config.toml` file of this app')
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
-st.code("""
-[theme]
-primaryColor="#F39C12"
-backgroundColor="#2E86C1"
-secondaryBackgroundColor="#AED6F1"
-textColor="#FFFFFF"
-font="monospace"
-""")
+# And the root-level secrets are also accessible as environment variables:
 
-number = st.sidebar.slider('Select a number:', 0, 10, 5)
-st.write('Selected number from slider widget is:', number)
+import os
+st.write(
+	"Has environment variables been set:",
+	os.environ["db_username"] == st.secrets["db_username"])
